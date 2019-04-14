d12e1de158970422ec43df77a5bf276b9437249d
Mariusz Ozdoba <mariuszozdoba@e902ab1f.local>
2019-04-14 20:01:09 +0200

    allowed hosts

--- golf_scores/golf_scores/settings.py
+++ golf_scores/golf_scores/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '98ywpw9(pgm&#$1ol2cmviy2qbdi!m#yc@(y6%+6p$+jkyyl8#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 # Application definition
 
